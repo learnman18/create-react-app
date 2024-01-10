73687d0ba26085c671c5366a89d2bdaf25144449
learnman18 <manishpan18@gmail.com>
2024-01-11 00:26:22 +0530

    [11-1]-newsAPi

---
+++ src/components/webPage/ClassComp/Article.js
@@ -0,0 +1,265 @@
+
+    const articles =  [
+      {
+        "source": {
+          "id": null,
+          "name": "Moneycontrol"
+        },
+        "author": "Reuters",
+        "title": "Apple’s Vision Pro AR headset to go on sale in US from February 2 - Moneycontrol",
+        "description": "",
+        "url": "https://www.moneycontrol.com/news/business/apples-vision-pro-ar-headset-to-go-on-sale-in-february-12018301.html",
+        "urlToImage": "https://images.moneycontrol.com/static-mcnews/2024/01/Apple-Vision-Pro-770x433.jpg",
+        "publishedAt": "2024-01-08T14:31:01Z",
+        "content": "Apple said on Monday its Vision Pro mixed reality device will be available in the United States from February 2.\r\nThe Vision Pro, which starts at $3,499, can be pre-ordered starting Jan. 19 at 5 am P… [+154 chars]"
+      },
+      {
+        "source": {
+          "id": "the-times-of-india",
+          "name": "The Times of India"
+        },
+        "author": "TIMESOFINDIA.COM",
+        "title": "What is Tayberry? What makes this rare berry so unique? - IndiaTimes",
+        "description": "Berries are known for their rich antioxidant properties that help in improving cell regeneration and reducing the impact of oxidative stress on the bo",
+        "url": "https://timesofindia.indiatimes.com/life-style/food-news/what-is-tayberry-what-makes-this-rare-berry-so-unique/articleshow/106639724.cms",
+        "urlToImage": "https://static.toiimg.com/thumb/msid-106640031,width-1070,height-580,imgsize-95106,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+        "publishedAt": "2024-01-08T14:31:00Z",
+        "content": "10 worst dressed celebs at Golden Globe Awards 2024"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Hindustan Times"
+        },
+        "author": "Shobhit Gupta",
+        "title": "Ayodhya Trust shares photos of Ram Janmabhoomi Temple adorned in nightlight - Hindustan Times",
+        "description": "The temple trust often shares photos showing the current status of the Ram Janmabhoomi temple. | Latest News India",
+        "url": "https://www.hindustantimes.com/india-news/ayodhya-trust-shares-photos-of-ram-janmabhoomi-temple-adorned-in-nightlight-see-here-101704705753532.html",
+        "urlToImage": "https://www.hindustantimes.com/ht-img/img/2024/01/08/1600x900/GDTVQpIb0AAyoy1_1704705775573_1704705783313.jpg",
+        "publishedAt": "2024-01-08T13:48:31Z",
+        "content": "Ahead of the consecration ceremony of the Ram temple in Ayodhya on January 22, Sri Ram Janmabhoomi Tirath Kshetra Trust on Monday shared a few pictures of the construction of the temple bathed in nig… [+2346 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "India Today"
+        },
+        "author": "Daphne Clarance",
+        "title": "Why do heart attack cases surge in winter? Experts explain - India Today",
+        "description": "When the temperature drops, our bodies respond to it by narrowing the blood vessels to conserve heat. This event leads to a heart attack in a person with cardiac issues.",
+        "url": "https://www.indiatoday.in/health/story/why-do-heart-attack-cases-surge-in-winter-experts-explain-2485971-2024-01-08",
+        "urlToImage": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202401/cold-weather-can-raise-your-risk-of-heart-and-circulatory-problems--such-as-heart-attacks-and-stroke-083745553-16x9_0.jpg?VersionId=cHgDo9GqO6Rw66GuenI8U8zf7X2SmdYM",
+        "publishedAt": "2024-01-08T13:42:24Z",
+        "content": "As the winter chill sets in, the human body undergoes various health complications besides respiratory illnesses. One of the most significant events witnesses by experts during winter is the rise in … [+3603 chars]"
+      },
+      {
+        "source": {
+          "id": "the-times-of-india",
+          "name": "The Times of India"
+        },
+        "author": "TIMESOFINDIA.COM",
+        "title": "Samsung unveils new range of QLED, Micro LED, OLED and Lifestyle displays ahead of CES 2024 - Times of India",
+        "description": "Samsung Electronics announces latest lineup of QLED, MICRO LED, OLED, and Lifestyle displays ahead of CES 2024. Introduces next-generation AI processo",
+        "url": "https://timesofindia.indiatimes.com/gadgets-news/samsung-unveils-new-range-of-qled-micro-led-oled-and-lifestyle-displays-ahead-of-ces-2024/articleshow/106642774.cms",
+        "urlToImage": "https://static.toiimg.com/thumb/msid-106642774,width-1070,height-580,imgsize-47984,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+        "publishedAt": "2024-01-08T13:36:15Z",
+        "content": "5G smartphones with 10GB or more RAM under Rs 25,000"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Hindustan Times"
+        },
+        "author": "Mallika Soni",
+        "title": "Israel kills Hezbollah commander Wissam Hassan Tawi in Lebanon: What we know - Hindustan Times",
+        "description": "Israel killed a top commander of Hezbollah group in a strike on south Lebanon as fears rise that the war in Gaza could spread in Middle east. | World News",
+        "url": "https://www.hindustantimes.com/world-news/israel-kills-hezbollah-commander-wissam-hassan-tawi-in-lebanon-101704719462209.html",
+        "urlToImage": "https://www.hindustantimes.com/ht-img/img/2024/01/08/1600x900/newspress-collage-2uc4iafab-1704716263195_1704719560130_1704719566573.webp",
+        "publishedAt": "2024-01-08T13:14:25Z",
+        "content": "Israel killed a top commander of Hezbollah group Wissam Hassan Tawil in a strike on south Lebanon as fears rise that the war in Gaza could spread in Middle east. The Israeli attack comes amid escalat… [+1757 chars]"
+      },
+      {
+        "source": {
+          "id": "the-hindu",
+          "name": "The Hindu"
+        },
+        "author": "The Hindu",
+        "title": "Rohit Sharma to lead India's T20I side against Afghanistan; Virat Kohli included - The Hindu",
+        "description": null,
+        "url": "https://www.thehindu.com/sport/cricket/rohit-sharma-to-lead-indias-t20i-side-against-afghanistan-virat-kohli-included/article67716504.ece",
+        "urlToImage": null,
+        "publishedAt": "2024-01-08T13:04:00Z",
+        "content": null
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "The Indian Express"
+        },
+        "author": "The Indian Express",
+        "title": "Golden Globes 2024 Live Updates: In an Oppenheimer sweep, Christopher Nolan, Cillian Murphy, Robert Downey Jr nab big wins - The Indian Express",
+        "description": null,
+        "url": "https://indianexpress.com/article/entertainment/hollywood/golden-globes-2024-live-updates-oppenheimer-barbie-9098983/",
+        "urlToImage": null,
+        "publishedAt": "2024-01-08T12:55:00Z",
+        "content": null
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Wisden.com"
+        },
+        "author": "Naman Agarwal",
+        "title": "'Our Groundsmen Do It Deliberately, Theirs Just Get It Wrong' - Sunil Gavaskar On Pitch Debate Double Standards | SA Vs IND | Cricket News Today - Wisden",
+        "description": "Sunil Gavaskar has hit out at the double standards in the debate surrounding pitches worldwide. Click here to read more.",
+        "url": "https://wisden.com/series-stories/south-africa-v-india-2023-24/our-groundsmen-do-it-deliberately-theirs-just-get-it-wrong-sunil-gavaskar-on-pitch-debate-double-standards",
+        "urlToImage": "https://cdn.wisden.com/wp-content/uploads/2024/01/gava.png",
+        "publishedAt": "2024-01-08T12:22:36Z",
+        "content": "Sunil Gavaskar has hit out at the double standards in the debate surrounding pitches worldwide.\r\nIndia won the second Test of their series against South Africa at Newlands, Cape Town. On a spicy surf… [+3431 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Livemint"
+        },
+        "author": "Subhash Narayan",
+        "title": "Govt announces ₹45000 crore investment to develop river cruise tourism | Mint - Mint",
+        "description": "The plan includes expanding river cruise tourism from eight to 26 waterways and increasing cruise circuits with night stays from 17 to 80. The number of river cruise terminals is set to rise from 15 to 185, expecting a significant boost in cruise tourism traf…",
+        "url": "https://www.livemint.com/news/govt-announces-rs-45000-crore-investment-to-develop-river-cruise-tourism-11704713584704.html",
+        "urlToImage": "https://www.livemint.com/lm-img/img/2024/01/08/1600x900/Cargo_Ship_1644076001520_1704715763148.JPG",
+        "publishedAt": "2024-01-08T12:12:07Z",
+        "content": "New Delhi: The ministry of ports, shipping, and waterways has announced a 45,000 crore investment to develop river cruise tourism, aiming to leverage India's inland waterways for economic growth. Thi… [+2925 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "NDTV News"
+        },
+        "author": null,
+        "title": "\"India Won't Tolerate...\": Lakshadweep Administrator Praful Patel On Maldives Row - NDTV",
+        "description": "Lakshadweep row: The remarks by three Maldives ministers - which included comments critical of Prime Minister Narendra Modi - \"challenged the dignity of India\", Lakshadweep Administrator Praful Khoda Patel told NDTV Monday.",
+        "url": "https://www.ndtv.com/india-news/india-maldives-row-lakshadweep-india-wont-tolerate-lakshadweep-administrator-on-maldives-row-4822289",
+        "urlToImage": "https://c.ndtvimg.com/2024-01/i15bk9o_lakshadweep-administrator-praful-patel_625x300_08_January_24.jpg",
+        "publishedAt": "2024-01-08T12:06:00Z",
+        "content": "Lakshadweep row: Praful Khoda Patel also waved away talk of demanding a public apology from Maldives.\r\nNew Delhi: The remarks by three Maldives ministers - which included comments critical of Prime M… [+2551 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Hindustan Times"
+        },
+        "author": "HT Entertainment Desk",
+        "title": "Fighter song Heer Aasmani: Hrithik, Deepika and gang all set to take flight - Hindustan Times",
+        "description": "The third song from Siddharth Anand's aerial action film Fighter is an ode to air force pilots. It features Hrithik Roshan and Deepika Padukone among others. | Bollywood",
+        "url": "https://www.hindustantimes.com/entertainment/bollywood/fighter-song-heer-aasmani-hrithik-roshan-deepika-padukone-gang-all-set-to-take-flight-watch-101704713277303.html",
+        "urlToImage": "https://www.hindustantimes.com/ht-img/img/2024/01/08/1600x900/fighter_1704714347590_1704714377670.jpg",
+        "publishedAt": "2024-01-08T11:54:52Z",
+        "content": "After the songs Sher Khul Gaye and Ishq Jaisa Kuch, the makers of Siddharth Anand's upcoming aerial action entertainer Fighter have unveiled the next song, Heer Aasmani. It features the lead pair, Hr… [+2741 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Hindustan Times"
+        },
+        "author": "Neha Yadav",
+        "title": "NASA, ISRO, SpaceX: Top 6 space agencies in the world - Hindustan Times",
+        "description": "NASA, ISRO, SpaceX: Top 6 space agencies in the world",
+        "url": "https://www.hindustantimes.com/web-stories/in-focus/nasa-isro-spacex-top-6-space-agencies-in-the-world-101704708174898.html",
+        "urlToImage": null,
+        "publishedAt": "2024-01-08T11:40:38Z",
+        "content": "By Neha YadavPublished Jan 08, 2024\r\nHindustan TimesIn FocusPhoto Credits: Unsplash"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "The Indian Express"
+        },
+        "author": "The Indian Express",
+        "title": "Congress suggests forming ‘common secretariat’ with AAP to fight Lok Sabha polls - The Indian Express",
+        "description": null,
+        "url": "https://indianexpress.com/article/cities/delhi/aap-congress-lok-sabha-polls-2024-seat-sharing-9100271/",
+        "urlToImage": null,
+        "publishedAt": "2024-01-08T11:23:36Z",
+        "content": null
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Thehealthsite.com"
+        },
+        "author": "Satata Karmakar",
+        "title": "JN.1 Variant Becomes Dominant Strain In India INSACOG Warns of Unusual COVID-19 Symptoms | TheHealthSite. - TheHealthSite",
+        "description": "The recent spike in COVID cases is being attributed to the JN.1 variant which has become the dominant strain in India as per INSACOG. TheHealthSite.com",
+        "url": "https://www.thehealthsite.com/news/jn-1-variant-becomes-dominant-strain-in-india-insacog-warns-of-unusual-covid-19-symptoms-1052144/",
+        "urlToImage": "https://st1.thehealthsite.com/wp-content/uploads/2024/01/JN.1-Variant-Becomes-Dominant-Strain-In-India-INSACOG-Warns-of-Unusual-COVID-19-Symptoms.jpg",
+        "publishedAt": "2024-01-08T11:23:00Z",
+        "content": "JN.1 Variant Becomes Dominant Strain In India, INSACOG Warns of Unusual COVID-19 Symptoms\r\nThe recent spike in COVID cases is being attributed to the JN.1 variant, which has become the dominant strai… [+4829 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Hindustan Times"
+        },
+        "author": "Mallika Soni",
+        "title": "China on India-Maldives row: ‘Never asked them to reject New Delhi’ - Hindustan Times",
+        "description": "China on India-Maldives Row: China has always treated Maldives as an equal partner and respected its sovereignty, the editorial by Global Times said. | World News",
+        "url": "https://www.hindustantimes.com/world-news/india-maldives-controversy-never-asked-them-to-reject-india-china-on-maldives-ministers-remarks-101704711804454.html",
+        "urlToImage": "https://www.hindustantimes.com/ht-img/img/2024/01/08/1600x900/PTI01-08-2024-000069B-0_1704712532929_1704712559924.jpg",
+        "publishedAt": "2024-01-08T11:19:05Z",
+        "content": "China's state media referred to New Delhi's diplomatic row with the Male in an editorial as Maldives' president Mohamed Muizzu landed in Beijing to meet his Chinese counterpart Xi Jinping and sign bi… [+2108 chars]"
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "Livemint"
+        },
+        "author": "Devesh Kumar",
+        "title": "MakeMyTrip observes 3400% increase in Lakshadweep searches amid India-Maldives row | Mint - Mint",
+        "description": "The development comes after a diplomatic row erupted between India and the Maldives over derogatory remarks by Maldives minister Mariyam Shiuna against Prime Minister Narendra Modi.",
+        "url": "https://www.livemint.com/news/india/makemytrip-observes-3400-increase-in-lakshadweep-searches-amid-india-maldives-row-11704710001713.html",
+        "urlToImage": "https://www.livemint.com/lm-img/img/2024/01/08/1600x900/3_1704710953260_1704710955818.jpg",
+        "publishedAt": "2024-01-08T11:06:52Z",
+        "content": "Travel company MakeMyTrip (MMT) on Monday informed that it has observed a 3400% increase in on-platform searches for Lakshadweep since the visit of Prime Minister Narendra Modi to the Union Territory… [+2745 chars]"
+      },
+      {
+        "source": {
+          "id": "the-times-of-india",
+          "name": "The Times of India"
+        },
+        "author": "etimes.in",
+        "title": "Ranveer Singh deletes post after he mistakenly shares a picture of Maldives to promote Lakshadweep touris - IndiaTimes",
+        "description": "Urging exploration of Lakshadweep's beauty, Ranveer Singh, too, advocated domestic tourism. However, his tweet featured a 2016 Maldives picture instea",
+        "url": "https://timesofindia.indiatimes.com/entertainment/hindi/bollywood/news/ranveer-singh-deletes-post-after-he-mistakenly-shares-a-picture-of-maldives-to-promote-lakshadweep-tourism/articleshow/106637589.cms",
+        "urlToImage": "https://static.toiimg.com/thumb/msid-106637589,width-1070,height-580,imgsize-42866,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+        "publishedAt": "2024-01-08T10:52:21Z",
+        "content": "The Train to Bhavyaah: 10 power packed performances of Sheena Chohan"
+      },
+      {
+        "source": {
+          "id": "the-times-of-india",
+          "name": "The Times of India"
+        },
+        "author": "TOI Business Desk",
+        "title": "Stock market today: BSE Sensex plunges 670 points, Nifty50 below 21,600 - top reasons why bears are playi - IndiaTimes",
+        "description": "India Business News: Stock market today: Indian equity markets faced a decline on Monday, with the benchmark Sensex dropping more than 650 points and falling below 71,400",
+        "url": "https://timesofindia.indiatimes.com/business/india-business/stock-market-today-bse-sensex-nifty50-top-reasons-for-dalal-street-plunging-on-january-8-2024/articleshow/106637026.cms",
+        "urlToImage": "https://static.toiimg.com/thumb/msid-106637268,width-1070,height-580,imgsize-104782,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+        "publishedAt": "2024-01-08T10:46:16Z",
+        "content": null
+      },
+      {
+        "source": {
+          "id": null,
+          "name": "YouTube"
+        },
+        "author": null,
+        "title": "Moon-bound Vulcan rocket successfully launches into space - Guardian News",
+        "description": "The Peregrine 1 lander carrying Nasa scientific equipment is on its way to the moon after a successful launch of the Vulcan Centaur rocket at Cape Canaveral....",
+        "url": "https://www.youtube.com/watch?v=t7sX2R73E68",
+        "urlToImage": "https://i.ytimg.com/vi/t7sX2R73E68/maxresdefault.jpg",
+        "publishedAt": "2024-01-08T10:41:35Z",
+        "content": null
+      }
+    ]
+  
+export default articles;
--- src/components/webPage/ClassComp/News.js
+++ src/components/webPage/ClassComp/News.js
@@ -1,33 +1,77 @@
 import React, {Component} from "react";
-
+// import Articles from "./Article"
 class News extends Component{
 
     constructor(){
-        console.log("it will be rendered first");
+        console.log("constructor");
         super();
         this.state = {
-            articles:[]
+            articles:[],
+            page:1,
+            // pageSize:15,
+            // articleLength:""
+            // articles:Articles
         }
     }
+
 
 //first render gets renders first then didMount gets renderd
 
     async componentDidMount(){
-        console.log("it will be renderd after render");
-        const url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=356f95ea33764cdbade0c4f3f02a9764";
-        const response = await fetch(url);
-        const result = await response.json();
+        console.log("cdm");
+        // let url = ""
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=356f95ea33764cdbade0c4f3f02a9764&page=1&pageSize=20";
+        let response = await fetch(url);
+        let result = await response.json();
         console.log(result)
         this.setState({
-            articles: result.articles
+            articles: result.articles,
+            totalResult : result.totalResults
         })
     }
+   
+//please refer classNote.txt for info on increment/decremnt settate
+
+    increment = async () => {
+        if( this.state.page + 1 > Math.ceil(this.state.totalResult/20)){
+
+        }
+        else{
+            // let url = ""
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=356f95ea33764cdbade0c4f3f02a9764&page=${this.state.page + 1}&pageSize=20`;
+            let response = await fetch(url);
+            let result = await response.json();
+            console.log(result)
+            this.setState({
+                articles: result.articles,
+                totalResult : result.totalResults,
+                page : this.state.page + 1
+            })
+            // console.log("increment" , this.state.page)
+        }
+    }
+  
+    decrement = async () => {
+        // let url = ""
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=356f95ea33764cdbade0c4f3f02a9764&page=${this.state.page - 1}&pageSize=20`;
+        let response = await fetch(url);
+        let result = await response.json();
+        console.log(result)
+        this.setState({
+            articles: result.articles,
+            totalResult : result.totalResults,
+            page : this.state.page - 1
+        })
+        console.log("decremnt" , this.state.page)
+    }
+
 
     render(){
-        console.log("it will be render before didMount but after constructor");
+        console.log("render");
+        // console.log("artcle", this.state.articles)
         return(
             <>
-                {
+                { 
                     this.state.articles.map((elem)=>{
                         return(
                         <div className="col-md-4 d-flex align-items-stretch" key={elem.url}>
@@ -44,7 +88,11 @@ class News extends Component{
                         </div>
                         )
                     })
-                }    
+                }
+                <div className="d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} className="btn btn-primary" id="previous" onClick={this.decrement}>Previous</button>
+                    <button className="btn btn-primary" id="next" onClick={this.increment}>Next</button>
+                </div>
             </>
         )
     }
